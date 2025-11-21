bd7e5d925f0997c10879ac702493d41abacff7fd
hbohlen <bohlenhayden@gmail.com>
2025-11-21 16:53:28 -0600

    feat(server-impermanence): complete integration module
    - Add universal Btrfs mount options
    - Configure auto-scrub for data integrity
    - Include optimized settings for server workloads

--- modules/shared/filesystems/server-btrfs.nix
+++ modules/shared/filesystems/server-btrfs.nix
@@ -7,6 +7,71 @@
   ];
   
   config = {
-    # Btrfs configuration will be completed in Task 1.2
+    # Universal Btrfs mount options for servers
+    fileSystems = {
+      "/" = {
+        options = [
+          "compress=zstd:3"
+          "noatime"
+          "ssd"
+          "space_cache=v2"
+          "discard=async"
+          "subvol=root"
+        ];
+      };
+      
+      "/nix" = {
+        options = [
+          "compress=zstd:1"
+          "noatime"
+          "ssd"
+          "space_cache=v2"
+          "discard=async"
+          "subvol=nix"
+        ];
+      };
+      
+      "/persist" = {
+        options = [
+          "compress=zstd:3"
+          "noatime"
+          "ssd"
+          "space_cache=v2"
+          "discard=async"
+          "subvol=persist"
+        ];
+      };
+      
+      "/var/log" = {
+        options = [
+          "compress=zstd:3"
+          "noatime"
+          "ssd"
+          "space_cache=v2"
+          "discard=async"
+          "subvol=log"
+        ];
+      };
+      
+      "/var/lib/containers" = {
+        options = [
+          "compress=zstd:1"
+          "noatime"
+          "nodatacow"
+          "nodatasum"
+          "ssd"
+          "space_cache=v2"
+          "discard=async"
+          "subvol=containers"
+        ];
+      };
+    };
+    
+    # Enable Btrfs quota support for multi-tenant isolation
+    services.btrfs.autoScrub = {
+      enable = true;
+      interval = "weekly";
+      fileSystems = [ "/" ];
+    };
   };
 }
